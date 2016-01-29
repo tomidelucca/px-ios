64918a4d4b4669ee3ba2d1d5037ba8f8fa430892
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2016-01-29 16:24:31 -0300

    Initial commit

---
+++ MercadoPagoSDK/MercadoPagoSDK/PaymentMethodCell.swift
@@ -0,0 +1,24 @@
+//
+//  PaymentMethodCell.swift
+//  MercadoPagoSDK
+//
+//  Created by Maria cristina rodriguez on 29/1/16.
+//  Copyright © 2016 MercadoPago. All rights reserved.
+//
+
+import UIKit
+
+class PaymentMethodCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
