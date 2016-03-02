04b77a1cb775092de05b0b410425d4884cf68e12
Demian Tejo <demian.tejo@mercadolibre.com>
2016-03-02 14:05:25 -0300

    no message

--- MercadoPagoSDK/MercadoPagoSDK/MerchantPayment.swift
+++ MercadoPagoSDK/MercadoPagoSDK/MerchantPayment.swift
@@ -10,18 +10,18 @@ import Foundation
 
 public class MerchantPayment : NSObject {
     public var issuer : Issuer?
-    public var cardTokenId : String?
+    public var cardTokenId : String!
     public var campaignId : Int = 0
     public var installments : Int = 0
     public var items : [Item]
     public var merchantAccessToken : String!
     public var paymentMethod : PaymentMethod!
-
-  
-    public init(items: [Item], installments: Int, issuer: Issuer?, tokenId: String?, paymentMethod: PaymentMethod, campaignId: Int) {
+    
+    
+    public init(items: [Item], installments: Int, cardIssuer: Issuer?, tokenId: String, paymentMethod: PaymentMethod, campaignId: Int) {
         self.items = items
         self.installments = installments
-        self.issuer = issuer
+        self.issuer = cardIssuer
         self.cardTokenId = tokenId
         self.paymentMethod = paymentMethod
         self.campaignId = campaignId
@@ -30,11 +30,10 @@ public class MerchantPayment : NSObject {
     
     public func toJSONString() -> String {
         let obj:[String:AnyObject] = [
-            "card_issuer_id": self.issuer == nil ? JSON.null : (self.issuer)!,
+            "card_issuer_id": self.issuer?._id == 0 ? JSON.null : (self.issuer?._id)!,
             "card_token": self.cardTokenId == nil ? JSON.null : self.cardTokenId!,
             "campaign_id": self.campaignId == 0 ? JSON.null : String(self.campaignId),
-            //TODO : should be plural
-//            "items": JSON(self.items[0]),
+            //"item": JSON(self.items[0]),
             "installments" : self.installments == 0 ? JSON.null : self.installments,
             "merchant_access_token" : self.merchantAccessToken == nil ? JSON.null : self.merchantAccessToken!,
             "payment_method_id" : self.paymentMethod._id == nil ? JSON.null : self.paymentMethod._id!
