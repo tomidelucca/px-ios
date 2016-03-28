8a2ea2db9fedaa41931afc7f83599377ccec7c06
Demian Tejo <demian.tejo@mercadolibre.com>
2016-03-28 15:31:31 -0300

    Add MPLabel

---
+++ MercadoPagoSDK/MercadoPagoSDK/MPLabel.swift
@@ -0,0 +1,21 @@
+//
+//  MPLabel.swift
+//  MercadoPagoSDK
+//
+//  Created by Demian Tejo on 3/28/16.
+//  Copyright © 2016 MercadoPago. All rights reserved.
+//
+
+import UIKit
+
+class MPLabel: UILabel {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MercadoPagoSDK/MercadoPagoSDK/MPTextField.swift
@@ -0,0 +1,21 @@
+//
+//  MPTextField.swift
+//  MercadoPagoSDK
+//
+//  Created by Demian Tejo on 3/28/16.
+//  Copyright © 2016 MercadoPago. All rights reserved.
+//
+
+import UIKit
+
+class MPTextField: UITextField {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
