6ebace966d8438763d6d00738ee86144c7722f33
Demian Tejo <demian.tejo@mercadolibre.com>
2016-06-03 10:40:56 -0300

    - Ver igual que Android el codigo de seguridad -

--- MercadoPagoSDK/MercadoPagoSDK/CardBackView.swift
+++ MercadoPagoSDK/MercadoPagoSDK/CardBackView.swift
@@ -28,23 +28,24 @@ import UIKit
         let nib = UINib(nibName: "CardBackView", bundle: bundle)
         let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
         view.frame = bounds
-        let screenSize: CGRect = UIScreen.mainScreen().bounds
-        let screenHeight = screenSize.height
+       // let screenSize: CGRect = UIScreen.mainScreen().bounds
+       // let screenHeight = screenSize.height
         view.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
-        self.addSubview(view);
-             let circlePath = UIBezierPath(arcCenter:cardCVV.center, radius: CGFloat(screenHeight*0.05), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+        self.addSubview(view)
         
-        let shapeLayer = CAShapeLayer()
-        shapeLayer.path = circlePath.CGPath
+        //let circlePath = UIBezierPath(arcCenter:cardCVV.center, radius: CGFloat(screenHeight*0.05), startAngle: CGFloat(0), endAngle:CGFloat(M_PI * 2), clockwise: true)
+        
+       // let shapeLayer = CAShapeLayer()
+       // shapeLayer.path = circlePath.CGPath
         
         //change the fill color
-        shapeLayer.fillColor = UIColor.clearColor().CGColor
+       // shapeLayer.fillColor = UIColor.clearColor().CGColor
         //you can change the stroke color
-        shapeLayer.strokeColor = UIColor.redColor().CGColor
+       // shapeLayer.strokeColor = UIColor.redColor().CGColor
         //you can change the line width
-        shapeLayer.lineWidth = 4.0
+       // shapeLayer.lineWidth = 4.0
         
-        cardCVV.layer.addSublayer(shapeLayer)
+      //  cardCVV.layer.addSublayer(shapeLayer)
         cardCVV.numberOfLines = 0
         
     }
