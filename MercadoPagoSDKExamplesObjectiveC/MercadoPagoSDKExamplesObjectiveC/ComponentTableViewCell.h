cd5f052858ad16fd2476dda565d77702b29149f4
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2016-06-30 14:27:18 -0300

    Examples objective-C

---
+++ MercadoPagoSDKExamplesObjectiveC/MercadoPagoSDKExamplesObjectiveC/ComponentTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  ComponentTableViewCell.h
+//  MercadoPagoSDKExamplesObjectiveC
+//
+//  Created by Maria cristina rodriguez on 30/6/16.
+//  Copyright © 2016 MercadoPago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ComponentTableViewCell : UITableViewCell
+
+- (void)initWith:( NSString * _Nonnull )title iconName:( NSString * _Nonnull )iconName;
+
+@end
