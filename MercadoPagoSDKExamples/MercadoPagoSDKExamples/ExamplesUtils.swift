4b4aa5cb0f64c238c557c851245322130496948a
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2016-07-07 15:58:17 -0300

    Unify PK

--- MercadoPagoSDKExamples/MercadoPagoSDKExamples/ExamplesUtils.swift
+++ MercadoPagoSDKExamples/MercadoPagoSDKExamples/ExamplesUtils.swift
@@ -19,8 +19,8 @@ class ExamplesUtils {
 		// "2b66598b-8b0f-4588-bd2f-c80ca21c6d18" // VZ
 		// "aa371283-ad00-4d5d-af5d-ed9f58e139f1" // CO
         
-       // "444a9ef5-8a6b-429f-abdf-587639155d88"
-          "APP_USR-5a399d42-6015-4f6a-8ff8-dd7d368068f8"
+       "444a9ef5-8a6b-429f-abdf-587639155d88"
+          //"APP_USR-5a399d42-6015-4f6a-8ff8-dd7d368068f8"
  
         // "TEST-d7ecb23b-8cbd-4292-96d5-eccfe39748b5"
        // "TEST-2edbc541-4e19-43b9-8241-2cda72da6b6f"
--- MercadoPagoSDKExamples/MercadoPagoSDKExamples/MainExamplesViewController.swift
+++ MercadoPagoSDKExamples/MercadoPagoSDKExamples/MainExamplesViewController.swift
@@ -64,6 +64,7 @@ class MainExamplesViewController: UIViewController, UITableViewDataSource, UITab
         self.tableExamples.deselectRowAtIndexPath(indexPath, animated: true)
         switch indexPath.row {
         case 0:
+            MercadoPagoContext.setPublicKey(ExamplesUtils.MERCHANT_PUBLIC_KEY_TEST)
             //Checkout Example
             let choFlow = MPFlowBuilder.startCheckoutViewController(ExamplesUtils.PREF_ID_NO_EXCLUSIONS, callback: { (payment: Payment) in
                 
