4426bfa3e98e2888706ad3ae5ce33f6bb92f1c25
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2016-08-31 11:09:15 -0300

    Common MP_OP_ENV variable

--- MercadoPagoSDK/MercadoPagoSDK/MercadoPago.swift
+++ MercadoPagoSDK/MercadoPagoSDK/MercadoPago.swift
@@ -63,6 +63,8 @@ public class MercadoPago : NSObject, UIAlertViewDelegate {
     static let MP_PROD_ENV = "/v1"
     static let MP_ENVIROMENT = MP_PROD_ENV  + "/checkout"
     
+    static let MP_OP_ENVIROMENT = "/v1"
+    
     static let MP_API_BASE_URL : String = "https://api.mercadopago.com"
     static let MP_CUSTOMER_URI = "/customers?preference_id="
     static let MP_PAYMENTS_URI = MP_ENVIROMENT + "/payments"
