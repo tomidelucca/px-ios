661052ad415d02973b7faef66f4b776d273c976b
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2016-12-15 18:06:21 -0300

    Update version 2.0.3.4

--- MercadoPagoSDK/MercadoPagoSDK/MercadoPagoContext.swift
+++ MercadoPagoSDK/MercadoPagoSDK/MercadoPagoContext.swift
@@ -65,7 +65,7 @@ public class MercadoPagoContext : NSObject, MPTrackerDelegate {
         return  "iOS"
     }
     public func sdkVersion() -> String!{
-        return "2.0.3.1"
+        return "2.0.3.4"
     }
  
     static let siteIdsSettings : [String : NSDictionary] = [
