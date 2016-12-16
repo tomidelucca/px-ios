d73a422f25bf0b3964a650c67a2049148a0d23ed
Demian Tejo <demian.tejo@mercadolibre.com>
2016-12-16 07:57:46 -0300

    - Fix   Font  let font = CGFontCreateWithDataProvider(provider)

--- MercadoPagoSDK/MercadoPagoSDK/MercadoPagoUIViewController.swift
+++ MercadoPagoSDK/MercadoPagoSDK/MercadoPagoUIViewController.swift
@@ -55,11 +55,13 @@ public class MercadoPagoUIViewController: UIViewController, UIGestureRecognizerD
                 var error: Unmanaged<CFError>?
                 let cfdata = CFDataCreate(nil, UnsafePointer<UInt8>(inData.bytes), inData.length)
                 if let provider = CGDataProviderCreateWithCFData(cfdata) {
-                    let font = CGFontCreateWithDataProvider(provider)
+                    if let font = CGFontCreateWithDataProvider(provider){
                         if (!CTFontManagerRegisterGraphicsFont(font, &error)) {
                             print("Failed to load font: \(error)")
                         }
                         return true
+                    }
+
                 }
             }
         }
