0d2db385711863e3d5a267352e6c5a6ac63b5a76
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2016-12-16 12:46:35 -0300

    Compile fix

--- MercadoPagoSDK/MercadoPagoSDK/MercadoPagoUIViewController.swift
+++ MercadoPagoSDK/MercadoPagoSDK/MercadoPagoUIViewController.swift
@@ -56,10 +56,10 @@ public class MercadoPagoUIViewController: UIViewController, UIGestureRecognizerD
                 let cfdata = CFDataCreate(nil, UnsafePointer<UInt8>(inData.bytes), inData.length)
                 if let provider = CGDataProviderCreateWithCFData(cfdata) {
                     let font = CGFontCreateWithDataProvider(provider)
-                        if (!CTFontManagerRegisterGraphicsFont(font, &error)) {
-                            print("Failed to load font: \(error)")
-                        }
-                        return true
+                    if (!CTFontManagerRegisterGraphicsFont(font, &error)) {
+                        print("Failed to load font: \(error)")
+                    }
+                    return true
                 }
             }
         }
