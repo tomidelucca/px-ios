15de5b4f9242d56c0a4d7e274e0210ecc04d48af
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2017-01-05 10:57:00 -0300

    Fix #452 : inverted param in createPreference request

---
+++ MercadoPagoSDK/MercadoPagoSDKTests/ExtensionsTest.swift
@@ -0,0 +1,13 @@
+//
+//  ExtensionsTest.swift
+//  MercadoPagoSDK
+//
+//  Created by Maria cristina rodriguez on 1/5/17.
+//  Copyright © 2017 MercadoPago. All rights reserved.
+//
+
+import UIKit
+
+class ExtensionsTest: BaseTest {
+
+}
