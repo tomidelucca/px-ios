87f968dd304260424ace3c74290834fab11455f8
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2017-01-10 15:14:45 -0300

    Remove examples tests

--- MercadoPagoSDK/MercadoPagoSDKTests/MercadoPagoSDKTests.swift
+++ MercadoPagoSDK/MercadoPagoSDKTests/MercadoPagoSDKTests.swift
@@ -21,16 +21,5 @@ class MercadoPagoSDKTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
 }
