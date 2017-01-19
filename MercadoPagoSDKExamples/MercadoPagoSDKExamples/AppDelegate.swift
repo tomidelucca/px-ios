13088b474bd9978fc7a61d213f12a7d9b3937761
Eden Torres <eden.etorres@gmail.com>
2017-01-19 16:34:11 -0300

    Remove test font

--- MercadoPagoSDKExamples/MercadoPagoSDKExamples/AppDelegate.swift
+++ MercadoPagoSDKExamples/MercadoPagoSDKExamples/AppDelegate.swift
@@ -47,7 +47,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         let decorationPreference = DecorationPreference()
         decorationPreference.setBaseColor(color: UIColor.green)
-        //decorationPreference.setFontName(fontName: "Fipps")
         MercadoPagoContext.setDecorationPreference(decorationPreference: decorationPreference)
         
         CardFormViewController.showBankDeals = true
