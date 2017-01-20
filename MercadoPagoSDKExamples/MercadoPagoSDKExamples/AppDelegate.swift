931042f37d672f8466fa7b1740d067ee08abb685
Eden Torres <eden.etorres@gmail.com>
2017-01-20 12:59:15 -0300

    Change loading color

--- MercadoPagoSDKExamples/MercadoPagoSDKExamples/AppDelegate.swift
+++ MercadoPagoSDKExamples/MercadoPagoSDKExamples/AppDelegate.swift
@@ -43,7 +43,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         //Pinto de rojo el color primerio
 
-//        MercadoPagoContext.setupPrimaryColor(UIColor.black, complementaryColor: UIColor.black)
+        //MercadoPagoContext.setupPrimaryColor(UIColor.black, complementaryColor: UIColor.black)
+        let decorationPreference = DecorationPreference()
+        decorationPreference.setBaseColor(color: UIColor.red)
+        
+        MercadoPagoContext.setDecorationPreference(decorationPreference: decorationPreference)
+        
 
         MercadoPagoContext.setLightTextColor()
         MercadoPagoContext.setSite(MercadoPagoContext.Site.MLA)
