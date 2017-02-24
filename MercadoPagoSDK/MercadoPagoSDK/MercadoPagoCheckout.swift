a592b3bab8ee47eaba7f20c311887e582ff2dfdc
Cristina Rodriguez <cristina.rodriguez@mercadolibre.com>
2017-02-24 14:12:16 -0300

    Start PaymentVault when changing payment method

--- MercadoPagoSDK/MercadoPagoSDK/MercadoPagoCheckout.swift
+++ MercadoPagoSDK/MercadoPagoSDK/MercadoPagoCheckout.swift
@@ -234,11 +234,7 @@ open class MercadoPagoCheckout: NSObject {
         if self.viewModel.reviewAndConfirm {
             let checkoutVC = CheckoutViewController(viewModel: self.viewModel.checkoutViewModel(), callback: {(paymentData : PaymentData) -> Void in
                 self.viewModel.updateCheckoutModel(paymentData: paymentData)
-                if MercadoPagoCheckoutViewModel.paymentDataCallback != nil {
-                    self.executePaymentDataCallback()
-                } else {
                     self.executeNextStep()
-                }
             }, callbackCancel : { Void -> Void in
                 self.viewModel.setIsCheckoutComplete(isCheckoutComplete: true)
                 self.executeNextStep()
