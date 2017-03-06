06f58d011b26d23802a0f94a4c9158d97cf74e28
Demian Tejo <demian.tejo@mercadolibre.com>
2017-03-06 16:43:11 -0300

    Update Version Number

--- MercadoPagoSDK/MercadoPagoSDK/MercadoPagoContext.swift
+++ MercadoPagoSDK/MercadoPagoSDK/MercadoPagoContext.swift
@@ -79,7 +79,7 @@ open class MercadoPagoContext : NSObject, MPTrackerDelegate {
         return  "iOS"
     }
     open func sdkVersion() -> String!{
-        return "2.2.10"
+        return "2.2.11"
     }
  
     static let siteIdsSettings : [String : NSDictionary] = [
