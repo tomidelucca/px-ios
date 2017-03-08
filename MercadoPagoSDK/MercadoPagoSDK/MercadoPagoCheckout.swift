1edd8a4c5ec44d067b1e5e6eefc5ba4df37d88ad
Demian Tejo <demian.tejo@mercadolibre.com>
2017-03-08 13:02:04 -0300

    Push

--- MercadoPagoSDK/MercadoPagoSDK/MercadoPagoCheckout.swift
+++ MercadoPagoSDK/MercadoPagoSDK/MercadoPagoCheckout.swift
@@ -285,7 +285,7 @@ open class MercadoPagoCheckout: NSObject {
 	func cleanNavigationStack () {
 		
 		// TODO WALLET
-        var newNavigationStack = self.navigationController.viewControllers.filter {!$0.isKind(of:MercadoPagoUIViewController.self) || $0.isKind(of:CheckoutViewController.self);
+        var  newNavigationStack = self.navigationController.viewControllers.filter {!$0.isKind(of:MercadoPagoUIViewController.self) || $0.isKind(of:CheckoutViewController.self);
         }
         self.navigationController.viewControllers = newNavigationStack;
         
