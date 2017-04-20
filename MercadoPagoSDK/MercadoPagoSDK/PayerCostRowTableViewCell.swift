cdc302bb3cbaa1123b08d207c729067d95733d97
Augusto Collerone <collerone.augusto@gmail.com>
2017-04-20 15:18:34 -0300

    PayerCost FirstRow description Deleted

--- MercadoPagoSDK/MercadoPagoSDK/PayerCostRowTableViewCell.swift
+++ MercadoPagoSDK/MercadoPagoSDK/PayerCostRowTableViewCell.swift
@@ -15,7 +15,9 @@ class PayerCostRowTableViewCell: UITableViewCell {
     
     func fillCell(payerCost : PayerCost) {
         let currency = MercadoPagoContext.getCurrency()
-        if (payerCost.hasInstallmentsRate() || payerCost.installments == 1){
+        if payerCost.installments == 1 {
+            interestDescription.attributedText = NSAttributedString(string : "".localized)
+        } else if (payerCost.hasInstallmentsRate()){
             let attributedTotal = NSMutableAttributedString(attributedString: NSAttributedString(string: "(", attributes: [NSForegroundColorAttributeName : UIColor.px_grayLight()]))
             attributedTotal.append(Utils.getAttributedAmount(payerCost.totalAmount, currency: currency, color : UIColor.px_grayLight(), fontSize: 15, baselineOffset:3))
             attributedTotal.append(NSAttributedString(string: ")", attributes: [NSForegroundColorAttributeName : UIColor.px_grayLight()]))
