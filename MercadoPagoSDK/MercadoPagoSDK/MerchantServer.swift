1c7c36cb171b496e6600846df367e8f451028e85
Mauro Reverter <revertermauro@gmail.com>
2017-06-14 17:49:17 -0300

    Added payment in process handling

--- MercadoPagoSDK/MercadoPagoSDK/MerchantServer.swift
+++ MercadoPagoSDK/MercadoPagoSDK/MerchantServer.swift
@@ -54,7 +54,17 @@ open class MerchantServer: NSObject {
 
             if let paymentDic = jsonResult as? NSDictionary {
                 if paymentDic["error"] != nil {
-                    if failure != nil {
+                    if paymentDic["status"] as? Int == ApiUtil.StatusCodes.PROCESSING.rawValue {
+                        let inProcessPayment = Payment()
+                        inProcessPayment.status = PaymentStatus.IN_PROCESS.rawValue
+                        inProcessPayment.statusDetail = PendingStatusDetail.CONTINGENCY.rawValue
+                        let dicPayer = paymentBody["payer"] as? NSDictionary
+                        if dicPayer != nil {
+                            let payer = Payer.fromJSON(dicPayer!)
+                            inProcessPayment.payer = payer
+                        }
+                        success(inProcessPayment)
+                    } else if failure != nil {
                         failure!(NSError(domain: "mercadopago.sdk.merchantServer.createPayment", code: MercadoPago.ERROR_API_CODE, userInfo: paymentDic as! [AnyHashable: AnyObject]))
                     }
                 } else {
