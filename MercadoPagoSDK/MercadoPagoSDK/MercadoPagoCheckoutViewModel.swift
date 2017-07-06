93d700e6a6bec23b417b805039b9ca4ac4098565
Mauro Reverter <revertermauro@gmail.com>
2017-07-06 15:59:28 -0300

    Removed data cleaning which is fixed in another PR

--- MercadoPagoSDK/MercadoPagoSDK/MercadoPagoCheckoutViewModel.swift
+++ MercadoPagoSDK/MercadoPagoSDK/MercadoPagoCheckoutViewModel.swift
@@ -214,7 +214,6 @@ open class MercadoPagoCheckoutViewModel: NSObject {
 
     //SEARCH_PAYMENT_METHODS
     public func updateCheckoutModel(paymentMethods: [PaymentMethod], cardToken: CardToken?) {
-        self.paymentData.token = nil
         self.paymentMethods = paymentMethods
         self.paymentData.paymentMethod = self.paymentMethods?[0] // Ver si son mas de uno
         self.cardToken = cardToken
@@ -239,9 +238,6 @@ open class MercadoPagoCheckoutViewModel: NSObject {
     }
 
     public func updateCheckoutModel(identification: Identification) {
-        //TODO BORRAR LIMPADA!
-        self.paymentData.token = nil
-        //
         if paymentData.paymentMethod.isCard() {
             self.cardToken!.cardholder!.identification = identification
         } else {
