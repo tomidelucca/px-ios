ea9eb2983e5047b3ed553a766a537d1a5a0a86c0
demtej <demian.tejo+github@gmail.com>
2017-11-26 18:20:40 -0300

    add classes

---
+++ MercadoPagoSDK/MercadoPagoSDK/PXPaymentMethodBodyComponent.swift
@@ -0,0 +1,37 @@
+//
+//  PXPaymentMethodBodyComponent.swift
+//  MercadoPagoSDK
+//
+//  Created by Demian Tejo on 24/11/17.
+//  Copyright © 2017 MercadoPago. All rights reserved.
+//
+
+import UIKit
+
+class PXPaymentMethodBodyComponent: NSObject, PXComponetizable {
+    var props : PXPaymentMethodBodyComponentProps
+    
+    init(props : PXPaymentMethodBodyComponentProps) {
+       self.props = props
+    }
+    func render() -> UIView {
+        return PXPaymentMethodBodyComponentRenderer().render(component: self)
+    }
+}
+
+class PXPaymentMethodBodyComponentProps: NSObject {
+    var paymentMethodIcon: UIImage?
+    var amountTitle: String?
+    var amountDetail: String?
+    var paymentMethodDescription: String?
+    var paymentMethodDetail: String?
+    
+    init(paymentMethodIcon: UIImage?,amountTitle: String?,amountDetail: String?,paymentMethodDescription: String?,paymentMethodDetail: String?) {
+        self.paymentMethodIcon = paymentMethodIcon
+        self.amountTitle = amountTitle
+        self.amountDetail = amountDetail
+        self.paymentMethodDescription = paymentMethodDescription
+        self.paymentMethodDetail = paymentMethodDetail
+    }
+    
+}
---
+++ MercadoPagoSDK/MercadoPagoSDK/PXPaymentMethodBodyComponentRenderer.swift
@@ -0,0 +1,16 @@
+//
+//  PXPaymentMethodBodyComponentRenderer.swift
+//  MercadoPagoSDK
+//
+//  Created by Demian Tejo on 24/11/17.
+//  Copyright © 2017 MercadoPago. All rights reserved.
+//
+
+import UIKit
+
+class PXPaymentMethodBodyComponentRenderer: NSObject {
+
+    func render(component: PXPaymentMethodBodyComponent) -> UIView {
+        return UIView()
+    }
+}
