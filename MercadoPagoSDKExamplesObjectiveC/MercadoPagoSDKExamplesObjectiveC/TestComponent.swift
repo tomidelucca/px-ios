0f839b2fd4e9140afd07a02f430f6f1c620e5dac
demtej <demian.tejo+github@gmail.com>
2017-12-19 10:38:24 -0300

    Add TestComponent to example project, fixes to build

---
+++ MercadoPagoSDKExamplesObjectiveC/MercadoPagoSDKExamplesObjectiveC/TestComponent.swift
@@ -0,0 +1,13 @@
+//
+//  TestComponent.swift
+//  MercadoPagoSDKExamplesObjectiveC
+//
+//  Created by Demian Tejo on 19/12/17.
+//  Copyright © 2017 MercadoPago. All rights reserved.
+//
+
+import UIKit
+
+class TestComponent: NSObject {
+
+}
