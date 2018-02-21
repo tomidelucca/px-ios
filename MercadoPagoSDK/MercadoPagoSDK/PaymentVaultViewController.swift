f5d37a22bdcf2b314e6ec1bdf32d484beb25c592
Juan Sanzone <juan.sanzone@gmail.com>
2018-02-21 09:23:44 -0300

    Remove missing print

--- MercadoPagoSDK/MercadoPagoSDK/PaymentVaultViewController.swift
+++ MercadoPagoSDK/MercadoPagoSDK/PaymentVaultViewController.swift
@@ -91,10 +91,6 @@ open class PaymentVaultViewController: MercadoPagoUIScrollViewController, UIColl
     required  public init(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    deinit {
-        print("JUANSITO deinit")
-    }
 
     open override func viewDidLoad() {
         super.viewDidLoad()
