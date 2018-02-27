30d72fa35c0dafcd7dc62923cf0ef46eb16a1b8d
augustocollerone <collerone.augusto@gmail.com>
2018-02-27 11:25:53 -0300

    Fix paymentMethodDetail test

--- MercadoPagoSDK/MercadoPagoSDKTests/PXBodyComponentTest.swift
+++ MercadoPagoSDK/MercadoPagoSDKTests/PXBodyComponentTest.swift
@@ -25,7 +25,7 @@ class PXBodyComponentTest: BaseTest {
         XCTAssertEqual(paymentMethodView.amountTitle?.text, "$ 1.000")
         XCTAssertEqual(paymentMethodView.amountDetail?.text, nil)
         XCTAssertEqual(paymentMethodView.paymentMethodDescription?.text?.localized, "visa " + "terminada en ".localized + "1234")
-        XCTAssertEqual(paymentMethodView.paymentMethodDetail?.text, "name")
+        XCTAssertNil(paymentMethodView.paymentMethodDetail)
         XCTAssertEqual(paymentMethodView.disclaimerLabel?.text?.localized, "En tu estado de cuenta verás el cargo como %0".localized.replacingOccurrences(of: "%0", with: "description"))
     }
 
