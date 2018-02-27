9fd08c1e8875458e474d259a9ca93ff4f01f474b
demtej <demian.tejo+github@gmail.com>
2018-02-27 15:49:32 -0300

    PXReviewViewModel

---
+++ MercadoPagoSDK/MercadoPagoSDK/PXReviewViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  PXReviewViewModel.swift
+//  MercadoPagoSDK
+//
+//  Created by Demian Tejo on 27/2/18.
+//  Copyright © 2018 MercadoPago. All rights reserved.
+//
+
+import UIKit
+
+class PXReviewViewModel: NSObject {
+
+}
