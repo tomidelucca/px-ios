b609dd25ae13b88da900f551780ca2c24efc2425
Eden Torres <eden.etorres@gmail.com>
2018-03-01 14:55:14 -0300

    Init

---
+++ MercadoPagoSDK/MercadoPagoSDK/PXItemComponent.swift
@@ -0,0 +1,9 @@
+//
+//  PXItemComponent.swift
+//  MercadoPagoSDK
+//
+//  Created by Eden Torres on 3/1/18.
+//  Copyright © 2018 MercadoPago. All rights reserved.
+//
+
+import Foundation
---
+++ MercadoPagoSDK/MercadoPagoSDK/PXItemRenderer.swift
@@ -0,0 +1,9 @@
+//
+//  PXItemRenderer.swift
+//  MercadoPagoSDK
+//
+//  Created by Eden Torres on 3/1/18.
+//  Copyright © 2018 MercadoPago. All rights reserved.
+//
+
+import Foundation
