7bc704fe3c05d3065a3cef20277bf18ff631cf1e
Juan Sanzone <juan.sanzone@gmail.com>
2018-03-17 13:09:06 -0300

    Improve multilines summary compact

--- MercadoPagoSDK/MercadoPagoSDK/PXSummaryCompactComponentView.swift
+++ MercadoPagoSDK/MercadoPagoSDK/PXSummaryCompactComponentView.swift
@@ -9,8 +9,11 @@
 import Foundation
 
 final class PXSummaryCompactComponentView: PXComponentView {
+    static let TITLE_FONT_SIZE: CGFloat = PXLayout.XXXL_FONT
+    static let CUSTOM_TITLE_FONT_SIZE: CGFloat = PXLayout.XS_FONT
     
-    fileprivate let TOP_BOTTOM_MARGIN: CGFloat = PXLayout.L_MARGIN * 2
+    fileprivate let PERCENT_SCREEN_WIDTH: CGFloat = 86
+    fileprivate let TOP_BOTTOM_MARGIN: CGFloat = PXLayout.L_MARGIN
     fileprivate let INTER_MARGIN: CGFloat = 9
     
     fileprivate var totalLabel: UILabel?
@@ -19,23 +22,29 @@ final class PXSummaryCompactComponentView: PXComponentView {
 
 extension PXSummaryCompactComponentView {
     
-    func buildView(amountAttributeText: NSAttributedString, bottomCustomTitle: NSAttributedString?, textColor: UIColor, backgroundColor: UIColor) -> CGFloat {
+    func buildView(amountAttributeText: NSAttributedString, bottomCustomTitle: NSAttributedString, textColor: UIColor, backgroundColor: UIColor) -> CGFloat {
         
         self.backgroundColor = backgroundColor
         
+        let screenWidth = PXLayout.getScreenWidth(applyingMarginFactor: PERCENT_SCREEN_WIDTH)
+        
         let (tLabel, cLabel) = buildLabels(amountText: amountAttributeText, customText: bottomCustomTitle, in: self, textColor: textColor)
+        
+        let titleHeight = UILabel.requiredHeight(forAttributedText: amountAttributeText, withFont: Utils.getFont(size: PXSummaryCompactComponentView.TITLE_FONT_SIZE), inNumberOfLines: 1, inWidth: screenWidth)
+        PXLayout.setHeight(owner: tLabel, height: titleHeight).isActive = true
+        
+        let customTitleHeight = UILabel.requiredHeight(forAttributedText: bottomCustomTitle, withFont: Utils.getFont(size: PXSummaryCompactComponentView.CUSTOM_TITLE_FONT_SIZE), inNumberOfLines: 3, inWidth: screenWidth)
+        PXLayout.setHeight(owner: cLabel, height: customTitleHeight).isActive = true
         
         totalLabel = tLabel
         customTextLabel = cLabel
         
-        return tLabel.requiredAttributedHeight() + cLabel.requiredAttributedHeight() + INTER_MARGIN + TOP_BOTTOM_MARGIN
+        return titleHeight + customTitleHeight + INTER_MARGIN + TOP_BOTTOM_MARGIN*2
     }
     
     fileprivate func buildLabels(amountText: NSAttributedString?, customText: NSAttributedString?, in superView: UIView, textColor: UIColor) -> (UILabel, UILabel) {
         
-        let PERCENT_SCREEN_WIDTH: CGFloat = 95
         let amountLabel = UILabel()
-        
         amountLabel.translatesAutoresizingMaskIntoConstraints = false
         amountLabel.textAlignment = .center
         amountLabel.numberOfLines = 1
@@ -45,12 +54,13 @@ extension PXSummaryCompactComponentView {
         
         PXLayout.pinTop(view: amountLabel, to: superView, withMargin: PXLayout.L_MARGIN).isActive = true
         PXLayout.centerHorizontally(view: amountLabel).isActive = true
-        PXLayout.matchWidth(ofView: amountLabel).isActive = true
+        PXLayout.matchWidth(ofView: amountLabel, withPercentage: PERCENT_SCREEN_WIDTH).isActive = true
         
         let customTitleLabel = UILabel()
         customTitleLabel.translatesAutoresizingMaskIntoConstraints = false
         customTitleLabel.textAlignment = .center
-        customTitleLabel.numberOfLines = 2
+        customTitleLabel.numberOfLines = 3
+        customTitleLabel.lineBreakMode = .byTruncatingTail
         customTitleLabel.attributedText = customText
         customTitleLabel.textColor = textColor
         superView.addSubview(customTitleLabel)
