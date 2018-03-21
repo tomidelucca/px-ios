4bdb0a472272b9f858538f580082a0379ef7d437
Eden Torres <eden.etorres@gmail.com>
2018-03-21 13:42:37 -0300

    Remove old classes

--- MercadoPagoSDKExamplesObjectiveC/MercadoPagoSDKExamplesObjectiveC/MainExamplesViewController.m
+++ MercadoPagoSDKExamplesObjectiveC/MercadoPagoSDKExamplesObjectiveC/MainExamplesViewController.m
@@ -83,7 +83,7 @@
     dc.amountWithoutDiscount = 100;
     dc = nil;
 
-    self.pref._id = @"243962506-e8314fc2-7abc-42d0-bf07-325b1f861ca8";
+    self.pref._id = @"243966003-0812580b-6082-4104-9bce-1a4c48a5bc44";
 
     self.mpCheckout = [[MercadoPagoCheckout alloc] initWithPublicKey:@"TEST-e4bdd1cf-bcb2-43f7-b565-ed4c9ea25be7"
     accessToken:nil
