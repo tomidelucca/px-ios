88de9e8544bb70c74e13fde0dbfed25c13e480b2
Demian Tejo <demian.tejo@gmail.com>
2018-03-27 13:07:11 -0300

    update version number

--- scripts/createPod.sh
+++ scripts/createPod.sh
@@ -11,7 +11,7 @@ VERSION=$1
 PROJECT="MercadoPagoSDK"
 PODSPEC_FILE="$PROJECT.podspec"
 ## Default branch is master
-GIT_BRANCH="development"
+GIT_BRANCH="release/4.0.0.beta.15"
 
 if [ "$#" -eq 2 ]
   then
