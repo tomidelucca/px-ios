0fdd2b4711479c574af09c1eeac0f5a548880be8
Demian Tejo <demian.tejo@gmail.com>
2018-04-12 14:32:26 -0300

    Add finishCallback to dissmisloading. Fix present after dissmis loading.

--- MercadoPagoSDK/MercadoPagoSDK/MercadoPagoCheckoutScreens.swift
+++ MercadoPagoSDK/MercadoPagoSDK/MercadoPagoCheckoutScreens.swift
@@ -233,7 +233,6 @@ extension MercadoPagoCheckout {
     }
 
     func showErrorScreen() {
-        self.dismissLoading()
         let errorStep = ErrorViewController(error: MercadoPagoCheckoutViewModel.error, callback: nil, callbackCancel: {[weak self] in
 
             guard let strongSelf = self else {
@@ -248,7 +247,10 @@ extension MercadoPagoCheckout {
                 self.viewModel.errorCallback?()
             })
         }
-        self.navigationController.present(errorStep, animated: true, completion: {})
+        self.dismissLoading {
+            self.navigationController.present(errorStep, animated: true, completion: {})
+        }
+        
     }
 
     func showFinancialInstitutionsScreen() {
