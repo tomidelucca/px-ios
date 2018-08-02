507096fc10066e5bf2dafe9992970d093e443adf
Juan Sanzone <juan.sanzone@gmail.com>
2018-08-02 15:57:36 -0300

    Defautl theme revert

--- MercadoPagoSDK/MercadoPagoSDK/PXDefaultTheme.swift
+++ MercadoPagoSDK/MercadoPagoSDK/PXDefaultTheme.swift
@@ -31,7 +31,7 @@ extension PXDefaultTheme: PXTheme {
     }
 
     public func detailedBackgroundColor() -> UIColor {
-        return #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        return #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
     }
 
     public func statusBarStyle() -> UIStatusBarStyle {
