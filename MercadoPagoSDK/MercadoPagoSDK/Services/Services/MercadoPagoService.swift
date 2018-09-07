da0e8ffd9ff00ec759eaa0f54f393ea6836edabe
Eden <eden.etorres@gmail.com>
2018-09-07 14:36:16 -0300

    Alamofire working

--- MercadoPagoSDK/MercadoPagoSDK/Services/Services/MercadoPagoService.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Services/Services/MercadoPagoService.swift
@@ -19,7 +19,7 @@ internal class MercadoPagoService: NSObject {
     }
 
     internal func request(uri: String, params: String?, body: Data?, method: HTTPMethod, headers: [String: String]? = nil, cache: Bool = true, success: @escaping (_ data: Data) -> Void,
-                        failure: ((_ error: NSError) -> Void)?) {
+                          failure: ((_ error: NSError) -> Void)?) {
 
         let url = baseURL + uri
         var requesturl = url
@@ -30,75 +30,94 @@ internal class MercadoPagoService: NSObject {
 
         let Rurl = URL(string: requesturl)
         var request = URLRequest(url: Rurl!)
-        request.httpMethod = HTTPMethod.post.rawValue
-        //request.setValue("application/json; charset=UTF-8", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = method.rawValue
         request.httpBody = body
 
-        MercadoPagoSDKV4.request(requesturl, method: method).responseData { response in
-            print("Request: \(response.request)")
-            print("Response: \(response.response)")
-            print("Error: \(response.error)")
-            debugPrint("All Response Info: \(response)")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        if let sdkVersion = PXServicesURLConfigs.PX_SDK_VERSION {
+            let value = "PX/iOS/" + sdkVersion
+            request.setValue(value, forHTTPHeaderField: "User-Agent")
+        }
 
-            if let data = response.result.value, let utf8Text = String(data: data, encoding: .utf8) {
-                print("Data: \(utf8Text)")
-                success(data)
+        if let headers = headers {
+            for header in headers {
+                request.setValue(header.value, forHTTPHeaderField: header.key)
             }
         }
 
-//        let finalURL: NSURL = NSURL(string: requesturl)!
-//        let request: NSMutableURLRequest
-//        if cache {
-//            request  = NSMutableURLRequest(url: finalURL as URL,
-//                                           cachePolicy: .returnCacheDataElseLoad, timeoutInterval: MP_DEFAULT_TIME_OUT)
-//        } else {
-//            request = NSMutableURLRequest(url: finalURL as URL,
-//                                          cachePolicy: .useProtocolCachePolicy, timeoutInterval: MP_DEFAULT_TIME_OUT)
-//        }
-//
-//        #if DEBUG
-//            print("\n--REQUEST_URL: \(finalURL)")
-//        #endif
-//
-//        request.url = finalURL as URL
-//        request.httpMethod = method
-//        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-//        if let sdkVersion = PXServicesURLConfigs.PX_SDK_VERSION {
-//            let value = "PX/iOS/" + sdkVersion
-//            request.setValue(value, forHTTPHeaderField: "User-Agent")
-//        }
-//
-//        if headers !=  nil && headers!.count > 0 {
-//            for header in headers! {
-//                request.setValue(header.value, forHTTPHeaderField: header.key)
-//            }
-//        }
-//        if let body = body {
-//            #if DEBUG
-//                print("--REQUEST_BODY: \(body as! NSString)")
-//            #endif
-//            request.httpBody = body.data(using: String.Encoding.utf8)
-//        }
-//
-//        UIApplication.shared.isNetworkActivityIndicatorVisible = true
-//
-//        NSURLConnection.sendAsynchronousRequest(request as URLRequest, queue: OperationQueue.main) { (response: URLResponse?, data: Data?, error: Error?) in
-//            UIApplication.shared.isNetworkActivityIndicatorVisible = false
-//            if error == nil && data != nil {
-//                do {
-//                    #if DEBUG
-//                        print("--REQUEST_RESPONSE: \(String(data: data!, encoding: String.Encoding.utf8) as! NSString)\n")
-//                    #endif
-//                    success(data!)
-//                } catch {
-//
-//                    let e: NSError = NSError(domain: "com.mercadopago.sdk", code: NSURLErrorCannotDecodeContentData, userInfo: nil)
-//                    failure?(e)
-//                }
-//            } else {
-//                failure?(error! as NSError)
-//            }
-//        }
-//    }
-}
+        MercadoPagoSDKV4.request(request).responseData { response in
+            #if DEBUG
+            print("Request: \(String(describing: response.request))")
+            if let body = response.request?.httpBody {
+                print("Request Body: \(String(describing: String(data: body, encoding: .utf8)))")
+            }
+            print("Error: \(String(describing: response.error))")
+            #endif
+
+            if let data = response.result.value, let utf8Text = String(data: data, encoding: .utf8), response.error == nil {
+                print("Data: \(utf8Text)")
+                success(data)
+            } else if let error = response.error as NSError? {
+                failure?(error)
+            } else {
+                let error: NSError = NSError(domain: "com.mercadopago.sdk", code: NSURLErrorCannotDecodeContentData, userInfo: nil)
+                failure?(error)
+            }
+        }
+
+        //        let finalURL: NSURL = NSURL(string: requesturl)!
+        //        let request: NSMutableURLRequest
+        //        if cache {
+        //            request  = NSMutableURLRequest(url: finalURL as URL,
+        //                                           cachePolicy: .returnCacheDataElseLoad, timeoutInterval: MP_DEFAULT_TIME_OUT)
+        //        } else {
+        //            request = NSMutableURLRequest(url: finalURL as URL,
+        //                                          cachePolicy: .useProtocolCachePolicy, timeoutInterval: MP_DEFAULT_TIME_OUT)
+        //        }
+        //
+        //        #if DEBUG
+        //            print("\n--REQUEST_URL: \(finalURL)")
+        //        #endif
+        //
+        //        request.url = finalURL as URL
+        //        request.httpMethod = method
+        //        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        //        if let sdkVersion = PXServicesURLConfigs.PX_SDK_VERSION {
+        //            let value = "PX/iOS/" + sdkVersion
+        //            request.setValue(value, forHTTPHeaderField: "User-Agent")
+        //        }
+        //
+        //        if headers !=  nil && headers!.count > 0 {
+        //            for header in headers! {
+        //                request.setValue(header.value, forHTTPHeaderField: header.key)
+        //            }
+        //        }
+        //        if let body = body {
+        //            #if DEBUG
+        //                print("--REQUEST_BODY: \(body as! NSString)")
+        //            #endif
+        //            request.httpBody = body.data(using: String.Encoding.utf8)
+        //        }
+        //
+        //        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        //
+        //        NSURLConnection.sendAsynchronousRequest(request as URLRequest, queue: OperationQueue.main) { (response: URLResponse?, data: Data?, error: Error?) in
+        //            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        //            if error == nil && data != nil {
+        //                do {
+        //                    #if DEBUG
+        //                        print("--REQUEST_RESPONSE: \(String(data: data!, encoding: String.Encoding.utf8) as! NSString)\n")
+        //                    #endif
+        //                    success(data!)
+        //                } catch {
+        //
+        //                    let e: NSError = NSError(domain: "com.mercadopago.sdk", code: NSURLErrorCannotDecodeContentData, userInfo: nil)
+        //                    failure?(e)
+        //                }
+        //            } else {
+        //                failure?(error! as NSError)
+        //            }
+        //        }
+        //    }
+    }
 }
