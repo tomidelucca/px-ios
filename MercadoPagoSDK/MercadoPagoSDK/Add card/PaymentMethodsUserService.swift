d6e16687ef91843528e3f9cfef382517fc89be93
Diego Flores Domenech <diego.domenech@mercadolibre.com>
2018-09-27 18:00:46 -0300

    safe try

--- MercadoPagoSDK/MercadoPagoSDK/Add card/PaymentMethodsUserService.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Add card/PaymentMethodsUserService.swift
@@ -24,7 +24,7 @@ class PaymentMethodsUserService: MercadoPagoService {
                 let paymentMethods = try JSONDecoder().decode([PXPaymentMethod].self, from: data)
                 success(paymentMethods)
             }catch{
-                let apiException = try! PXApiException.fromJSON(data: data)
+                let apiException = try? PXApiException.fromJSON(data: data)
                 let dict = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any]
                 failure(PXError(domain: "mercadopago.sdk.getPaymentMethods", code: ErrorTypes.API_EXCEPTION_ERROR, userInfo: dict ?? [:], apiException: apiException))
             }
