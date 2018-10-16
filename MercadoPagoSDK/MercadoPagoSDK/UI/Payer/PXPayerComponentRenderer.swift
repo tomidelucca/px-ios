0381392360424f87d1b554803cec947ef8d1d553
Marcelo José <marcelo.jose@mercadolibre.com>
2018-10-16 15:32:10 -0300

    Set payer

---
+++ MercadoPagoSDK/MercadoPagoSDK/UI/Payer/PXPayerComponentRenderer.swift
@@ -0,0 +1,8 @@
+//
+//  PXPayerComponentRenderer.swift
+//  MercadoPagoSDK
+//
+//  Created by Marcelo Oscar José on 14/10/2018.
+//
+
+import Foundation
---
+++ MercadoPagoSDK/MercadoPagoSDK/UI/Payer/PXPayerView.swift
@@ -0,0 +1,8 @@
+//
+//  PXPayerView.swift
+//  MercadoPagoSDK
+//
+//  Created by Marcelo Oscar José on 14/10/2018.
+//
+
+import Foundation
