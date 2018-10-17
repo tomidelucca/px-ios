f9cf1bd7d02bf55d775fcca81fa08d04ce3008d6
Juan Sanzone <juan.sanzone@gmail.com>
2018-10-17 17:13:45 -0300

    Cards aligments

--- MercadoPagoSDK/MercadoPagoSDK/UI/PXCardSlider/PXMockedCards.swift
+++ MercadoPagoSDK/MercadoPagoSDK/UI/PXCardSlider/PXMockedCards.swift
@@ -29,7 +29,7 @@ import UIKit
     var cardPattern = [4, 4, 4, 4]
     var cardFontColor: UIColor = .white
     var cardLogoImage: UIImage? = ResourceManager.shared.getImage("visa_light")
-    var cardBackgroundColor: UIColor = UIColor(red: 75/255, green: 100/255, blue: 193/255, alpha: 1)
+    var cardBackgroundColor: UIColor = UIColor(red:0.13, green:0.22, blue:0.44, alpha:1.0)
     var securityCodeLocation: Location = .back
     var defaultUI = true
     var securityCodePattern = 3
@@ -44,7 +44,22 @@ import UIKit
     var cardPattern = [4, 4, 4, 4]
     var cardFontColor: UIColor = .white
     var cardLogoImage: UIImage? = ResourceManager.shared.getImage("debmaestro_light")
-    var cardBackgroundColor: UIColor = UIColor(red: 98/255, green: 132/255, blue: 153/255, alpha: 1)
+    var cardBackgroundColor: UIColor = UIColor(red:0.23, green:0.31, blue:0.39, alpha:1.0)
+    var securityCodeLocation: Location = .back
+    var defaultUI = true
+    var securityCodePattern = 3
+    var fontType: String = "light"
+}
+
+// Maestro
+@objc class Master: NSObject, CardUI {
+    var placeholderName = ""
+    var placeholderExpiration = ""
+    var bankImage: UIImage?
+    var cardPattern = [4, 4, 4, 4]
+    var cardFontColor: UIColor = .white
+    var cardLogoImage: UIImage? = ResourceManager.shared.getImage("master_light")
+    var cardBackgroundColor: UIColor = UIColor(red:0.23, green:0.31, blue:0.39, alpha:1.0)
     var securityCodeLocation: Location = .back
     var defaultUI = true
     var securityCodePattern = 3
@@ -60,6 +75,21 @@ import UIKit
     var cardFontColor: UIColor = .white
     var cardLogoImage: UIImage? = ResourceManager.shared.getImage("amex")
     var cardBackgroundColor: UIColor = UIColor(red: 238/255, green: 140/255, blue: 58/255, alpha: 1)
+    var securityCodeLocation: Location = .back
+    var defaultUI = true
+    var securityCodePattern = 3
+    var fontType: String = "light"
+}
+
+// Galicia Amex
+@objc class Amex: NSObject, CardUI {
+    var placeholderName = ""
+    var placeholderExpiration = ""
+    var bankImage: UIImage? = nil
+    var cardPattern = [4, 4, 4, 4]
+    var cardFontColor: UIColor = .white
+    var cardLogoImage: UIImage? = ResourceManager.shared.getImage("amex")
+    var cardBackgroundColor: UIColor = UIColor(red:0.45, green:0.64, blue:0.60, alpha:1.0)
     var securityCodeLocation: Location = .back
     var defaultUI = true
     var securityCodePattern = 3
