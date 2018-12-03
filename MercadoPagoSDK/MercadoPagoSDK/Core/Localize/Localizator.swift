b32da30f51cf9e7b612cf6f2b9295dadf2e176e3
Juan Sanzone <juan.sanzone@gmail.com>
2018-12-03 20:50:50 -0300

    Add verb support for money in

--- MercadoPagoSDK/MercadoPagoSDK/Core/Localize/Localizator.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Core/Localize/Localizator.swift
@@ -8,10 +8,22 @@
 
 import Foundation
 
+// Only for MoneyIn custom verb support.
+/// :nodoc
+public enum PXCustomTranslationKey: String {
+    case total_to_pay = "total_row_title_default"
+    case total_to_pay_onetap = "onetap_purchase_summary_total"
+    case how_to_pay = "¿Cómo quieres pagar?"
+}
+
 internal class Localizator {
     static let sharedInstance = Localizator()
     private var language: String = NSLocale.preferredLanguages[0]
+    private var customTrans: [PXCustomTranslationKey: String]?
+}
 
+// MARK: Privates.
+internal extension Localizator {
     private func localizableDictionary() -> NSDictionary? {
         let languageBundle = Bundle(path: getLocalizedPath())
         let languageID = getParentLanguageID()
@@ -33,30 +45,35 @@ internal class Localizator {
         }
         fatalError("Localizable file NOT found")
     }
-
 }
 
 // MARK: Getters/ Setters
 internal extension Localizator {
     func setLanguage(language: PXLanguages) {
         self.language = language.rawValue
+        self.customTrans = nil
     }
 
     func setLanguage(string: String) {
         self.language = string
+        self.customTrans = nil
     }
 
     func getLanguage() -> String {
         return language
     }
 
+    // MoneyIn Custom verb support.
+    func setLanguage(_ string: String, _ customTranslations: [PXCustomTranslationKey: String]) {
+        self.language = string
+        self.customTrans = customTranslations
+    }
 }
 
 // MARK: Localization Paths
 extension Localizator {
     private func getLocalizedID() -> String {
         let bundle = ResourceManager.shared.getBundle() ?? Bundle.main
-
         let currentLanguage = getLanguage()
         let currentLanguageSeparated = currentLanguage.components(separatedBy: "-").first
         if bundle.path(forResource: currentLanguage, ofType: "lproj") != nil {
@@ -104,19 +121,39 @@ extension Localizator {
 
         return localizedString
     }
+
+    func getCustomTrans(_ targetKey: String) -> String? {
+        if let cVerbs = customTrans {
+            for (key,value) in cVerbs {
+                if key.rawValue == targetKey {
+                    return value
+                }
+            }
+        }
+        return nil
+    }
 }
 
+// MARK: localized capability for Strings.
 internal extension String {
     var localized_beta: String {
+        if let customTrans = Localizator.sharedInstance.getCustomTrans(self) {
+            return customTrans
+        }
         return Localizator.sharedInstance.localize(string: self)
     }
 
     var localized: String {
+        if let customTrans = Localizator.sharedInstance.getCustomTrans(self) {
+            return customTrans
+        }
         var bundle: Bundle? = ResourceManager.shared.getBundle()
         if bundle == nil {
             bundle = Bundle.main
         }
-        let languageBundle = Bundle(path: Localizator.sharedInstance.getLocalizedPath())
-        return languageBundle!.localizedString(forKey: self, value: "", table: nil)
+        if let languageBundle = Bundle(path: Localizator.sharedInstance.getLocalizedPath()) {
+            return languageBundle.localizedString(forKey: self, value: "", table: nil)
+        }
+        return ""
     }
 }
