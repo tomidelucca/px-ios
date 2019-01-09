f740a019c7e162f4452daf84ec7817a761f409f9
Eden <eden.etorres@gmail.com>
2019-01-09 14:45:47 -0300

    Add split configuration model

--- MercadoPagoSDK/MercadoPagoSDK/Services/Models/PXAmountConfiguration.swift
+++ MercadoPagoSDK/MercadoPagoSDK/Services/Models/PXAmountConfiguration.swift
@@ -18,8 +18,10 @@ open class PXAmountConfiguration: NSObject, Codable {
         }
     }
     open var payerCosts: [PXPayerCost]?
+    open var sliptConfiguration: PXSplitConfiguration?
+    open var discountToken: Int64?
 
-    public init(selectedPayerCostIndex: Int?, payerCosts: [PXPayerCost]?) {
+    public init(selectedPayerCostIndex: Int?, payerCosts: [PXPayerCost]?, splitConfiguration: PXSplitConfiguration?, discountToken: Int64?) {
         self.selectedPayerCostIndex = selectedPayerCostIndex
         self.payerCosts = payerCosts
     }
@@ -27,19 +29,25 @@ open class PXAmountConfiguration: NSObject, Codable {
     public enum PXPayerCostConfiguration: String, CodingKey {
         case selectedPayerCostIndex = "selected_payer_cost_index"
         case payerCost = "payer_costs"
+        case split
+        case discountToken = "discount_token"
     }
 
     required public convenience init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: PXPayerCostConfiguration.self)
         let payerCosts: [PXPayerCost]? = try container.decodeIfPresent([PXPayerCost].self, forKey: .payerCost)
         let selectedPayerCostIndex: Int? = try container.decodeIfPresent(Int.self, forKey: .selectedPayerCostIndex)
-        self.init(selectedPayerCostIndex: selectedPayerCostIndex, payerCosts: payerCosts)
+        let splitConfiguration: PXSplitConfiguration? = try container.decodeIfPresent(PXSplitConfiguration.self, forKey: .split)
+        let discountToken: Int64? = try container.decodeIfPresent(Int64.self, forKey: .discountToken)
+        self.init(selectedPayerCostIndex: selectedPayerCostIndex, payerCosts: payerCosts, splitConfiguration: splitConfiguration, discountToken: discountToken)
     }
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: PXPayerCostConfiguration.self)
         try container.encodeIfPresent(self.payerCosts, forKey: .payerCost)
         try container.encodeIfPresent(self.selectedPayerCostIndex, forKey: .selectedPayerCostIndex)
+        try container.encodeIfPresent(self.discountToken, forKey: .discountToken)
+        try container.encodeIfPresent(self.sliptConfiguration, forKey: .split)
     }
 
     open func toJSONString() throws -> String? {
