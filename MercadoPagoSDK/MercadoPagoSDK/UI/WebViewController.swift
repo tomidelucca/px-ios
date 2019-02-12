193aed38b55cb88b649435cb7fcdf8a7d5e854f2
augustocollerone <collerone.augusto@gmail.com>
2019-02-12 12:23:29 -0300

    Present Discount terms & conditions

--- MercadoPagoSDK/MercadoPagoSDK/UI/WebViewController.swift
+++ MercadoPagoSDK/MercadoPagoSDK/UI/WebViewController.swift
@@ -10,18 +10,25 @@ import UIKit
 
 class WebViewController: MercadoPagoUIViewController, UIWebViewDelegate {
 
-    var url: URL?
+    var url: URL
     var name: String?
-    var navBarTitle: String!
-    @IBOutlet weak var webView: UIWebView!
+    var navBarTitle: String
+    let webView: UIWebView
+    let forceAddNavBar: Bool
     private var loadingVC: PXLoadingViewController
 
-    init( url: URL, screenName: String, navigationBarTitle: String) {
+    init(url: URL, screenName: String, navigationBarTitle: String, forceAddNavBar: Bool = false) {
         self.url = url
         self.name = screenName
         self.navBarTitle = navigationBarTitle
+        self.forceAddNavBar = forceAddNavBar
         self.loadingVC = PXLoadingViewController()
-        super.init(nibName: "WebViewController", bundle: ResourceManager.shared.getBundle())
+        self.webView = UIWebView(frame: .zero)
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @objc func close() {
+        self.dismiss(animated: true, completion: nil)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -30,8 +37,40 @@ class WebViewController: MercadoPagoUIViewController, UIWebViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.loadUrl(url!)
+        self.loadUrl(url)
+
         self.webView.delegate = self
+        self.webView.translatesAutoresizingMaskIntoConstraints = false
+        self.webView.backgroundColor = .white
+        self.view.addSubview(webView)
+        PXLayout.pinLeft(view: webView).isActive = true
+        PXLayout.pinRight(view: webView).isActive = true
+        PXLayout.pinBottom(view: webView).isActive = true
+
+        if self.navigationController == nil, forceAddNavBar {
+            let navBar: UINavigationBar = UINavigationBar(frame: CGRect(x: 0, y: PXLayout.getSafeAreaTopInset(), width: PXLayout.getScreenWidth(), height: 44))
+            self.view.addSubview(navBar);
+
+            navBar.isTranslucent = true
+            navBar.barTintColor = ThemeManager.shared.whiteColor()
+            navBar.tintColor = ThemeManager.shared.navigationBar().tintColor
+            navBar.backgroundColor = ThemeManager.shared.getMainColor()
+            navBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+
+            let textAttributes = [NSAttributedString.Key.foregroundColor:ThemeManager.shared.navigationBar().tintColor]
+            navBar.titleTextAttributes = textAttributes
+
+            let navItem = UINavigationItem(title: navBarTitle);
+            let closeImage = ResourceManager.shared.getImage("modal_close_button")?.ml_tintedImage(with: ThemeManager.shared.navigationBar().getTintColor())
+            let closeButton = UIBarButtonItem(image: closeImage, style: .plain, target: self, action: #selector(close))
+            navItem.leftBarButtonItem = closeButton;
+            navBar.setItems([navItem], animated: false);
+            PXLayout.put(view: webView, onBottomOf: navBar).isActive = true
+        } else {
+            PXLayout.pinTop(view: webView).isActive = true
+        }
+
+        self.view.backgroundColor = ThemeManager.shared.getMainColor()
         self.present(loadingVC, animated: false, completion: nil)
     }
 
